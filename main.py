08ae1dcd5a2def34bc4b94bdbcf9cb9a2a65945e
adiaxxa27-ctrl <adiaxxa27@gmail.com>
2026-09-17 16:00:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 TOKEN = os.environ.get("TELEGRAM_BOT_TOKEN")
-
+if TOKEN:
+    TOKEN = TOKEN.strip()
+print(f"Token loaded: {'Yes' if TOKEN else 'No'}")
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("Bot 24x7 ON hai ✅")
 
